a96cedf1a8b6faf67903daee9859d3d110a89df0
Tom <wiggywombathead@hotmail.co.uk>
2021-12-02 09:49:08 +0000

    Day 2 2021

---
+++ 2021/02.lisp
@@ -0,0 +1,24 @@
+(load "../2020/util.lisp")
+
+(defun advent-02a (filename)
+  (loop for (dir dst) in (mapcar #'str:words (get-file filename))
+        for direction = (read-from-string dir)
+        for distance = (parse-integer dst)
+        with (x y) = '(0 0)
+        do (case direction
+             (forward (incf x distance))
+             (down (incf y distance))
+             (up (decf y distance)))
+        finally (return (* x y))))
+
+(defun advent-02b (filename)
+  (loop for (dir dst) in (mapcar #'str:words (get-file filename))
+        for direction = (read-from-string dir)
+        for distance = (parse-integer dst)
+        with (x y aim) = '(0 0 0)
+        do (case direction
+             (forward (incf x distance)
+                      (incf y (* aim distance)))
+             (down (incf aim distance))
+             (up (decf aim distance)))
+        finally (return (* x y))))
