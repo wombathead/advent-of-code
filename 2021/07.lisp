0d172a5d66096c26a00d52beec43f89d8b7f99c0
Tom <wiggywombathead@hotmail.co.uk>
2021-12-19 00:56:22 +0000

    Day 8 2021

--- 2021/07.lisp
+++ 2021/07.lisp
@@ -10,16 +10,16 @@
            "Sum of absolute differences between POSITION and NUMBERS"
            (loop for n in numbers
                  sum (abs (- position n)))))
-    (let* ((numbers (get-numbers filename))
-           (y (median numbers)))
-      (loop for y in (if (integerp y) (list y) (list (floor y) (ceiling y)))
-        minimize (total-cost y numbers)))))
+    (loop with input = (get-numbers filename)
+          with m = (median input)
+          for y in (if (integerp m) (list m) (list (floor m) (ceiling m)))
+          minimize (total-cost y input))))
 
 (defun advent-07b (filename)
   (flet ((total-cost (position numbers)
            (loop for n in numbers
                  for difference = (abs (- position n))
                  sum (/ (* difference (1+ difference)) 2))))
-    (let ((input (get-numbers filename)))
-      (loop for i from (reduce #'min input) upto (reduce #'max input)
-        minimize (total-cost i input)))))
+    (loop with input = (get-numbers filename) 
+          for i from (reduce #'min input) upto (reduce #'max input)
+          minimize (total-cost i input))))
